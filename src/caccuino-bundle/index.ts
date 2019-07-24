d34958f9b2738470e9a02679adf43d059c859b29
Davide Bertola <dade@dadeb.it>
2019-07-24 17:21:38 +0200

    cleanup

--- src/caccuino-bundle/index.ts
+++ src/caccuino-bundle/index.ts
@@ -1,13 +1,12 @@
-//import './style/typebase.css'
 import './style/delta3.css'
 import 'markdown-it-highlight/dist/index.css'
 
-console.log(`Caccuino working directory: ${__dirname}`)
+console.log(`working directory: ${__dirname}`)
 
 // import pages from `./../**/*.md`
 var pages = require(`../**/*.md`)
 
-console.log(`Caccuino pages tree`, pages)
+console.log(`pages tree`, pages)
 
 const page = unescape(window.location.pathname)
   .substr(1)
@@ -18,6 +17,7 @@ const page = unescape(window.location.pathname)
 if (pages.menu) {
   document.querySelector(".menu").innerHTML = pages.menu.html
 }
+
 if (page) {
   document.querySelector(".content").innerHTML = page.html || "not found"
 } else {

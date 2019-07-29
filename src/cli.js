f5bc949947d49de2f6d297cb491a0231d2f40348
Davide Bertola <dade@dadeb.it>
2019-07-29 14:57:10 +0200

    avoid opening browser too often

--- src/cli.js
+++ src/cli.js
@@ -15,19 +15,19 @@ const entryFiles = path.join(bundleDir, './index.html')
 const options = {
   outDir: './caccuino-bundle/dist', // The out directory to put the build files in, defaults to dist
   publicUrl: '/', // The url to serve on, defaults to '/'
-  watch: false, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+  watch: true, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
   cache: true, // Enabled or disables caching, defaults to true
   cacheDir: './caccuino-bundle/cache', // The directory cache gets put in, defaults to .cache
   contentHash: false, // Disable content hash from being included on the filename
   global: 'moduleName', // Expose modules as UMD under this name, disabled by default
-  minify: false, // Minify files, enabled if process.env.NODE_ENV === 'production'
+  minify: true, // Minify files, enabled if process.env.NODE_ENV === 'production'
   scopeHoist: false, // Turn on experimental scope hoisting/tree shaking flag, for smaller production bundles
   target: 'browser', // Browser/node/electron, defaults to browser
   bundleNodeModules: true, // By default, package.json dependencies are not included when using 'node' or 'electron' with 'target' option above. Set to true to adds them to the bundle, false by default
   logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors
   hmr: true, // Enable or disable HMR while watching
   hmrPort: 0, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
-  sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
+  sourceMaps: false, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   hmrHostname: '', // A hostname for hot module reload, default to ''
   detailedReport: true, // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
   autoInstall: true, // Enable or disable auto install of missing dependencies found during bundling
@@ -75,8 +75,11 @@ ncp(sourceDir, bundleDir, (e) => {
   app.use(bundlerMiddleware)
   app.listen(8080)
   const open = require('open')
-  bundler.on('buildEnd', () => {
+  var started = false
+  bundler.on('bundled', () => {
+    if (started) { return }
     open(`http://${internalIp.v4.sync() || "localhost"}:8080`)
+    started = true
   })
 })
 

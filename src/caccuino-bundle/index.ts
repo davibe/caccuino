68d6cc4a8b625e13227d7e1eb103bf41b241919b
Davide Bertola <dade@dadeb.it>
2019-08-27 17:18:03 +0200

    prebundle app, don't touch dest dir, use apis to fetch MDs

--- src/caccuino-bundle/index.ts
+++ src/caccuino-bundle/index.ts
@@ -1,26 +1,23 @@
-import '../*.css'
 import 'markdown-it-highlight/dist/index.css'
 import render from './renderer'
 
-var pages = require(`../**/*.md`)
+// do this dynamically so parcel does not try to bundle the link
+const link = document.createElement('link')
+link.rel = "stylesheet"
+link.href = "/style.css"
+document.body.appendChild(link)
 
-const pageSource = unescape(window.location.pathname)
-  .substr(1)
-  .replace(".md", "")
-  .split('/')
-  .reduce((scope, acc) => {
-    return scope[acc]
-  }, pages)
+const doit = async () => {
+  const pagePath = unescape(window.location.pathname).substr(1)
+  const res = await fetch(`/___raw___/${pagePath}`)
+  const body = await res.text()
+  const page = render(body)
 
-const page = render(pageSource)
+  if (page) {
+    document.querySelector(".content").innerHTML = page.html || "not found"
+  } else {
+    document.querySelector(".content").innerHTML = "page not found"
+  }
 
-// TODO: outline ? filelist ? custom menu ?
-if (pages.menu) {
-  document.querySelector(".menu").innerHTML = pages.menu.html
 }
-
-if (page) {
-  document.querySelector(".content").innerHTML = page.html || "not found"
-} else {
-  document.querySelector(".content").innerHTML = "page not found"
-}
+doit()

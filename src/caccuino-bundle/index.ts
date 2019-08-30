6734dc1115e5184a34aaacaa262bc9c61de63b69
Davide Bertola <dade@dadeb.it>
2019-08-30 16:17:17 +0200

    first TOC/outline implementation

--- src/caccuino-bundle/index.ts
+++ src/caccuino-bundle/index.ts
@@ -10,11 +10,15 @@ document.body.appendChild(link)
 const doit = async () => {
   const pagePath = unescape(window.location.pathname).substr(1)
   const res = await fetch(`/___raw___/${pagePath}`)
-  const body = await res.text()
+  const body = await res.text() + "\n [[toc]]"
   const page = render(body)
 
   if (page) {
     document.querySelector(".content").innerHTML = page.html || "not found"
+    const tocEl = document.querySelector(".table-of-contents")
+    if (tocEl) {
+      tocEl.innerHTML = "<h4>Outline</h4>" + tocEl.innerHTML
+    }
   } else {
     document.querySelector(".content").innerHTML = "page not found"
   }

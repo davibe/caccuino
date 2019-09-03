01b6c33ebbdf541c3967637dfe21df09f20b89f0
Davide Bertola <dade@dadeb.it>
2019-09-03 09:33:55 +0200

    cleanup

--- src/walkDir.js
+++ src/walkDir.js
@@ -5,30 +5,30 @@ const ignoreDir = (d) => d.startsWith('.') || d.startsWith('node_modules')
 
 
 const walkDir = async (dir) => {
-  var obj = {}; // an object similar to { dir: { subdir: { subsub: {} } } }
-  var list = []; // a list similar to bash $ find .
+  var obj = {} // an object similar to { dir: { subdir: { subsub: {} } } }
+  var list = [] // a list similar to bash $ find .
 
   const files = await fs.readdir(dir, { withFileTypes: true })
 
   for (const dirEnt of files) {
     const f = dirEnt.name
     const isDirectory = dirEnt.isDirectory()
-    let dirPath = path.join(dir, f);
+    let dirPath = path.join(dir, f)
 
     if (isDirectory) {
       if (dir != '.' && ignoreDir(dir)) {
         continue
       }
-      const [obj2, list2] = await walkDir(dirPath);
-      obj[dirPath] = obj2;
+      const [obj2, list2] = await walkDir(dirPath)
+      obj[dirPath] = obj2
       list = list.concat(list2)
     } else {
-      obj[dirPath] = {};
+      obj[dirPath] = {}
       list = list.concat(dirPath)
     }
   }
 
-  return [obj, list];
+  return [obj, list]
 }
 
 
@@ -40,4 +40,4 @@ if (!module.parent) {
   })()
 }
 
-exports.walkDir = walkDir;
+exports.walkDir = walkDir
